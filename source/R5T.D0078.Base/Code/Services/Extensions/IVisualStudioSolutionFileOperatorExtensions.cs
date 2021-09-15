61fa5c5159607d626ae9640f7f401dae0c6d6c9f
David Coats <david@getsafetycone.com>
2021-09-15 15:25:18 -0700

    Add IVisualStudioSolutionFileOperator.AddProjectReferences() extension method.

--- source/R5T.D0078.Base/Code/Services/Extensions/IVisualStudioSolutionFileOperatorExtensions.cs
+++ source/R5T.D0078.Base/Code/Services/Extensions/IVisualStudioSolutionFileOperatorExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 using R5T.D0078;
@@ -8,6 +10,27 @@ namespace System
 {
     public static class IVisualStudioSolutionFileOperatorExtensions
     {
+        public static Task AddProjectReferences(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator,
+            string solutionFilePathToModify,
+            params string[] projectReferenceFilePathsToAdd)
+        {
+            return visualStudioSolutionFileOperator.AddProjectReferences(
+                solutionFilePathToModify,
+                projectReferenceFilePathsToAdd.AsEnumerable());
+        }
+
+        public static async Task AddProjectReferences(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator,
+            string solutionFilePathToModify,
+            IEnumerable<string> projectReferenceFilePathsToAdd)
+        {
+            foreach (var projectReferenceFilePath in projectReferenceFilePathsToAdd)
+            {
+                await visualStudioSolutionFileOperator.AddProjectReference(
+                    solutionFilePathToModify,
+                    projectReferenceFilePath);
+            }
+        }
+
         /// <summary>
         /// Selects <see cref="IVisualStudioSolutionFileOperator.RemoveProjectReferenceIdempotent(string, string)"/> as the default.
         /// </summary>
